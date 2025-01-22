09438d9fa8ff852ab551589b4589ebb323f9e3c7
mlnwns <129190157+mlnwns@users.noreply.github.com>
2025-01-22 16:57:28 +0900

    style: 신사 정보 및 칠복신 정보 (#23)
    * feat: Divider 컴포넌트 추가
    
    * feat: 결과 페이지에 Divider 컴포넌트 추가
    
    * feat: 결과 페이지에 신사 이름 및 칠복신 정보 추가

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,11 +1,24 @@
 import Shrine from "../../src/assets/Shrine/Asakusa.png";
+import Flower from "../assets/title_flower.png";
+import Ebisu from "../assets/Kami/Ebisu.png";
 import styled from "styled-components";
+import Divider from "../components/common/result/Divider";
+import colors from "../styles/color";
 
 const ResultPage = () => {
   return (
     <ResultLayout>
       <ShrineImg src={Shrine} alt="Shrine" />
       <DottedLine />
+      <Divider theme="light" />
+      <FlowerContainer>
+        <FlowerImg src={Flower} alt="Flower" />
+        <FlowerText className="font-pretendard">아사쿠사 신사</FlowerText>
+      </FlowerContainer>
+      <Divider theme="dark" />
+      <KamiImg src={Ebisu} alt="Kami" />
+      <KamiName className="font-pretendard">에비스</KamiName>
+      <Divider theme="light" />
     </ResultLayout>
   );
 };
@@ -27,9 +40,43 @@ const ShrineImg = styled.img`
   object-fit: contain;
 `;
 
+const FlowerContainer = styled.div`
+  display: flex;
+  align-items: center; /* 세로로 가운데 정렬 */
+  justify-content: center; /* 가로로 가운데 정렬 */
+  gap: 10px; /* 이미지와 텍스트 사이 간격 */
+  margin: 20px 0; /* 상하 간격 */
+`;
+
+const FlowerImg = styled.img`
+  width: 10%;
+  height: auto;
+  object-fit: contain;
+`;
+
+const KamiImg = styled.img`
+  width: 40%;
+  height: auto;
+  object-fit: contain;
+  margin-top: 30px;
+`;
+const FlowerText = styled.div`
+  font-size: 32px;
+  font-weight: bold;
+  color: ${colors.secondary};
+`;
+
+const KamiName = styled.div`
+  margin: 15px 0 20px 0;
+  font-size: 20px;
+  font-weight: bold;
+  color: ${colors.secondary};
+`;
+
 const DottedLine = styled.div`
+  position: relative;
+  left: -20px;
   width: calc(100% + 40px);
-  margin: 0 -20px;
   height: 2px;
   background-image: linear-gradient(to right, black 50%, transparent 50%);
   background-position: top;
