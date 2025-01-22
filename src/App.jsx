a14855e4c4edb83d5e33cbc47733e766d1e61dc7
kanghaeun <xgkg0330@jnu.ac.kr>
2025-01-22 16:56:42 +0900

    feat: 결과페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Container from "./components/common/Layouts/container";
+import ResultPage from "./pages/ResultPage";
 
 function App() {
   return (
     <Container>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/ResultPage" element={<ResultPage />} />
       </Routes>
     </Container>
   );
