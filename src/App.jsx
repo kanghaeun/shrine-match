cb6afd608e957abcf95567afe4cbb341e99cdbe8
Da-Yeon-Kim <210941@jnu.ac.kr>
2025-01-22 15:15:55 +0900

    feat: router 추가 및 home 화면 추가
     #12

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,11 @@
-import { useEffect, useState } from "react";
-import { supabase } from "./supabase";
-import Container from "./components/common/Layouts/container";
+import { Routes } from "react-router-dom";
+import HomePage from "./pages/Home";
 
 function App() {
-  const [questions, setQuestions] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchQuestions = async () => {
-      console.log("Fetching started");
-      const { data, error } = await supabase.from("questions").select("*");
-      console.log("Raw response:", { data, error });
-
-      if (error) {
-        console.error("Error:", error);
-        setError(error.message);
-      } else {
-        console.log("Data received:", data);
-        setQuestions(data);
-      }
-      setIsLoading(false);
-    };
-
-    fetchQuestions();
-  }, []);
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-
   return (
-    <Container>
-      <h1>Questions</h1>
-      <ul>
-        {questions.map((question) => (
-          <li key={question.id}>
-            {question.question_ko} - {question.question_en} -{" "}
-            {question.question_jp}
-          </li>
-        ))}
-      </ul>
-    </Container>
+    <Routes>
+      <Route path="/" element={HomePage />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div>
+      <p>homepage</p>
+    </div>
+  );
+}
+
+export default HomePage;
