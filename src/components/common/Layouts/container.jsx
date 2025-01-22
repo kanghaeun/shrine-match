7501ad12afc3f195260d3d9dba57beccea8bd9ed
Da-Yeon-Kim <210941@jnu.ac.kr>
2025-01-22 14:52:30 +0900

    chore: 충돌 해결

--- src/components/common/Layouts/container.jsx
+++ src/components/common/Layouts/container.jsx
@@ -1,8 +1,5 @@
 import styled from "styled-components";
-<<<<<<< HEAD
-=======
 import colors from "../../../styles/color";
->>>>>>> e0277cd5677614c533aa02b5f6e39fd0dbc0d20d
 
 function Container({ children }) {
   return (
@@ -19,13 +16,6 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-<<<<<<< HEAD
-`;
-
-const Inner = styled.div`
-  min-height: 100vh;
-  max-width: 380px;
-=======
   background-color: ${colors.background};
 `;
 
@@ -33,16 +23,11 @@ const Inner = styled.div`
   max-width: 500px;
   width: 100%;
   height: 972px;
->>>>>>> e0277cd5677614c533aa02b5f6e39fd0dbc0d20d
   display: flex;
   flex-direction: column;
   justify-content: left;
   align-items: flex-start;
   gap: 0;
   padding: 20px;
-<<<<<<< HEAD
-  background-color: #bbbbbb;
-=======
   background-color: white;
->>>>>>> e0277cd5677614c533aa02b5f6e39fd0dbc0d20d
 `;
