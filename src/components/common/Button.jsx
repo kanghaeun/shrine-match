5df342c82b3bec973b5564e58dda1f2d5a78e57d
Da-Yeon-Kim <210941@jnu.ac.kr>
2025-01-24 13:45:25 +0900

    chore: button 충돌 해결
     #12

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,24 +1,12 @@
 import styled from "styled-components";
 import colors from "../../styles/color";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-function Button({ children, ...props }) {
-  return(
-    <StyledButton {...props}>{children}</StyledButton>
-  );
-=======
-function Button({ children, className }) {
-  return <StyledButton className={className}>{children}</StyledButton>;
->>>>>>> 479ba27 (feat: button 스타일링)
-=======
 function Button({ children, className, ...props }) {
   return (
     <StyledButton className={className} {...props}>
       {children}
     </StyledButton>
   );
->>>>>>> 426f6db (feat: 가져온 데이터 상태에 저장 및 페이지 이동)
 }
 
 export default Button;
@@ -33,17 +21,5 @@ const StyledButton = styled.button`
   color: white;
   margin: 5px auto;
   font-weight: bold;
-<<<<<<< HEAD
-<<<<<<< HEAD
-  font-size: 1rem;
-=======
   cursor: pointer;
-
-  :hover {
-    background-color: ${colors.primary};
-  }
->>>>>>> 426f6db (feat: 가져온 데이터 상태에 저장 및 페이지 이동)
 `;
-=======
-`;
->>>>>>> 479ba27 (feat: button 스타일링)
