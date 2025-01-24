874da8620005f69d763c489d2527095a585e0625
LBB <202780@jnu.ac.kr>
2025-01-24 14:21:29 +0900

    refactor: 공통 컴포넌트 Button 대체 코드로 변경

--- src/components/QuestionPage/ChoiceButtons.jsx
+++ src/components/QuestionPage/ChoiceButtons.jsx
@@ -3,11 +3,18 @@ import Button from "../common/button";
 
 function ChoiceButtons({ choice, onSelect }){
   return (
-    <Button onClick={() => {alert("선택지 클릭!")}}>
+    <ChoiceButton onClick={() => {alert("선택지 클릭!")}}>
       울창한 대나무 숲길:
       바람이 대나무 사이를 스치며 편안한 소리를 냅니다.
-    </Button>
+    </ChoiceButton>
   );
 }
 
 export default ChoiceButtons;
+
+
+const ChoiceButton = styled(Button)`
+  height: auto;
+  font-size: 18px;
+  width: 100%;
+`;
